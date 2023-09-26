5d4c90e97432797f86991f54ba2cec02a0c74bd7
Rasul Kireev <rasul.kireev@guycarp.com>
2023-09-26 09:53:11 -0400

    mdx  works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
-  output: "server",
+  integrations: [
+      tailwind(),
+      vue(),
+      mdx()
+  ],
+  output: "hybrid",
   adapter: vercel()
 });
