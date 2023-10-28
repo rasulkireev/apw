5e02327b0101ca6ef70362ed6596ba3ec306f077
Rasul Kireev <me@rasulkireev.com>
2023-10-28 15:26:17 -0400

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import vercel from "@astrojs/vercel/serverless";
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rasulkireev.com",
-  integrations: [tailwind(), vue(), mdx(), react()],
+  integrations: [tailwind(), vue(), mdx(), react(), sitemap()],
   output: "hybrid",
   adapter: vercel()
 });
